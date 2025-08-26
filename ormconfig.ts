b7060e6cb1ea7e09c126b96207a3f34431502a92
AskaSlip <varenaska@gmail.com>
2025-08-26 13:25:36 +0400

    project backend orm

--- ormconfig.ts
+++ ormconfig.ts
@@ -10,8 +10,8 @@ const config = configuration().database;
 export default new DataSource({
     type: 'postgres',
     url: config?.url,
-    entities: [path.join(process.cwd(), 'dist', 'database', 'entities', '*.js')],
-    migrations: [path.join(process.cwd(), 'dist', 'database', 'migrations', '*.js')],
+    entities: [path.join(process.cwd(), 'dist', 'src', 'database', 'entities', '*.js')],
+    migrations: [path.join(process.cwd(), 'dist', 'src', 'database', 'migrations', '*.js')],
     synchronize: true,
 
 });
